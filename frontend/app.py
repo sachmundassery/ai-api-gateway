ec354a58fde9c8ec1032f578c2037b6ca4c42171
sachmundassery <69303859+sachmundassery@users.noreply.github.com>
2026-05-07 14:29:52 +0530

    updated for integrating railway app for fast api deployment

--- frontend/app.py
+++ frontend/app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import httpx
 import time
 
-BASE_URL = "http://localhost:8000"
+import os
+BASE_URL = os.getenv("BACKEND_URL", "http://localhost:8000")
 
 st.set_page_config(
     page_title="AI API Gateway",
